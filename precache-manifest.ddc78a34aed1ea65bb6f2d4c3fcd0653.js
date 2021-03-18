674b0aca0c56a355806c160b7f582f64436e06a4
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-18 18:47:17 +0200

    Updates

---
+++ precache-manifest.ddc78a34aed1ea65bb6f2d4c3fcd0653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8409106948a97124263ef97ca798424",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1568f0552b5c306fa6e",
+    "url": "./static/css/main.0ec1067f.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1568f0552b5c306fa6e",
+    "url": "./static/js/main.0c53f895.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
